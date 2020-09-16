66dbb4a62eb6826b87ae7f9a4b714fee60cc701e
gmliang <gmliang@nradiowifi.com>
2020-09-16 12:04:28 +0800

    NPT-87 归集subtask

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ function setColor(str, level) {
   return tmpStr;
 }
 
+function setLink(key, str) {
+  return `[${str || key}](https://nradio.atlassian.net/browse/${key})`;
+}
+
 function launch() {
   const baseTime = moment().format('YYYY-MM-DD');
   const searchOptions = {
@@ -36,25 +40,63 @@ function launch() {
     const { issues } = res;
     let str = `# ${moment().format('YYYY-MM-DD')} worklog\n`;
     let issueNum = 1;
+    const issueMap = new Map();
     issues.forEach((issue) => {
       const { worklogs } = issue.fields.worklog;
       const { summary } = issue.fields;
-      const { key } = issue;
-      let totalWorklog = 0;
-      let subWorklog = '';
+      let { key } = issue;
+      const issueKey = key;
+      if (!issue.fields.issuetype.subtask) {
+        if (!issueMap.has(key)) {
+          const initObj = {
+            issueSummary: summary,
+            totalWorklog: 0,
+            worklogs: [],
+          };
+          issueMap.set(key, initObj);
+        }
+      }
+      if (issue.fields.parent) {
+        key = issue.fields.parent.key;
+        if (!issueMap.has(key)) {
+          const initObj = {
+            issueSummary: issue.fields.parent.fields.summary,
+            totalWorklog: 0,
+            worklogs: [],
+          };
+          issueMap.set(key, initObj);
+        }
+      }
+      const issueInfo = issueMap.get(key);
       worklogs.forEach((worklog) => {
         // eslint-disable-next-line max-len
         const isAfter = moment(worklog.started).isAfter(moment(baseTime).subtract(RANGE_NUM, TIME_RANGE));
         if (isAfter) {
-          totalWorklog += worklog.timeSpentSeconds;
-          subWorklog += `- ${worklog.author.displayName} ${worklog.comment || ''}  ${setColor(worklog.timeSpent)}\n`;
+          const worklogInfo = {
+            author: worklog.author.displayName,
+            key: issueKey,
+            summary,
+            comment: worklog.comment,
+            timeSpentSeconds: worklog.timeSpentSeconds,
+            timeSpent: worklog.timeSpent,
+          };
+          issueInfo.worklogs.push(worklogInfo);
         }
       });
-      str += `${issueNum}. [${key}](https://nradio.atlassian.net/browse/${key})  **${summary}**  ${setColor(`${totalWorklog / 3600}h`)}\n`;
+    });
+    issueMap.forEach((issueData, key) => {
+      let subWorklog = '';
+      const tmpIssue = { ...issueData };
+      const { worklogs } = tmpIssue;
+      worklogs.forEach((worklog) => {
+        const isSubtask = worklog.key !== key;
+        tmpIssue.totalWorklog += worklog.timeSpentSeconds;
+        subWorklog += `- ${isSubtask ? `${setLink(key)} /` : ''} ${setLink(worklog.key)} ${setLink(key, worklog.summary)} ${worklog.author} ${worklog.comment || ''}  ${setColor(worklog.timeSpent)}\n`;
+      });
+      str += `${issueNum}. ${setLink(key)}  **${tmpIssue.issueSummary}**  ${setColor(`${tmpIssue.totalWorklog / 3600}h`)}\n`;
       str += subWorklog;
       issueNum += 1;
     });
-
     // 不能超过4096个字节
     wechat.sendMsg(str);
     dingding.sendMsg(str);
